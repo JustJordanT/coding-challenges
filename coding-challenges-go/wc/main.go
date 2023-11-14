ce42ca01519c09c3827647db39484452ec1a7710
Jordan <Jordantay9014@outlook.com>
2023-11-14 15:42:20 -0600

    wip/go-wc - completed wc

--- coding-challenges-go/wc/main.go
+++ coding-challenges-go/wc/main.go
@@ -4,120 +4,68 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"strings"
+	"unicode/utf8"
 )
 
-func CheckErr(e error) {
-	if e != nil {
-		log.Fatal("Error: ", e)
-	}
+type Count struct {
+	lines, words, chars, bytes int
 }
 
 func main() {
-
-	readBytes := flag.Bool("c", false, "reads bytes from file")
-	readLines := flag.Bool("l", false, "read lines from file")
-	readWords := flag.Bool("w", false, "read words from file")
-	readCharacters := flag.Bool("m", false, "read characters from file")
+	countLines := flag.Bool("l", false, "Count lines")
+	countWords := flag.Bool("w", false, "Count words")
+	countChars := flag.Bool("m", false, "Count characters")
+	countBytes := flag.Bool("c", false, "Count bytes")
 	flag.Parse()
 
-	var arg string
-
-	if !*readBytes && !*readLines && !*readWords && !*readCharacters {
-		arg = os.Args[1]
+	files := flag.Args()
+	if len(files) == 0 {
+		count := wc(os.Stdin)
+		printCount(count, *countLines, *countWords, *countChars, *countBytes)
 	} else {
-		arg = os.Args[2]
-	}
-
-	//Make sure at least one argument is provided
-	if len(arg) < 2 {
-		fmt.Println("No file specified")
-		return
-	}
-
-	if *readBytes {
-		fmt.Println(readBytesFunc(arg), arg)
-	}
-
-	if *readLines {
-		fmt.Println(readLinesFunc(arg), arg)
-	}
-
-	if *readWords {
-		fmt.Println(readWordsFunc(arg), arg)
-	}
-
-	if *readCharacters {
-		fmt.Println(readCharactersFunc(arg), arg)
-	}
-
-	if !*readBytes && !*readLines && !*readWords && !*readCharacters {
-		fmt.Printf("%v %v %v - %v\n", readLinesFunc(arg), readWordsFunc(arg), readBytesFunc(arg), arg)
+		for _, filename := range files {
+			file, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error opening file %s: %v\n", filename, err)
+				continue
+			}
+			count := wc(file)
+			file.Close()
+			printCount(count, *countLines, *countWords, *countChars, *countBytes)
+		}
 	}
 }
 
-func readCharactersFunc(arg string) int {
-	return len(readFileToString(arg))
-}
+func wc(reader io.Reader) Count {
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
 
-func readBytesFunc(arg string) int64 {
-	file, err := os.Stat(arg)
-	CheckErr(err)
-	return file.Size()
-}
-
-func readLinesFunc(arg string) int {
-	file, done := openFile(arg)
-	if done {
-		return 0
-	}
-
-	defer func(file *os.File) {
-		err := file.Close()
-		CheckErr(err)
-	}(file)
-
-	// Create a new Scanner to read the file
-	scanner := bufio.NewScanner(file)
-	lineCount := 0
-
-	// Read each line and count
+	var count Count
 	for scanner.Scan() {
-		lineCount++
+		count.lines++
+		line := scanner.Text()
+		count.words += len(strings.Fields(line))
+		count.bytes += len(line) // Count bytes
+		count.chars += utf8.RuneCountInString(line)
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading from file:", err)
+	return count
+}
+
+func printCount(count Count, countLines, countWords, countChars, countBytes bool) {
+	if countLines {
+		fmt.Printf("Lines: %d\n", count.lines)
 	}
-
-	return lineCount
-}
-
-func readWordsFunc(arg string) int {
-	fileStrings := readFileToString(arg)
-	fields := strings.Fields(fileStrings)
-
-	return len(fields)
-}
-
-func readFileToString(filePath string) string {
-	file, err := os.ReadFile(filePath)
-	CheckErr(err)
-
-	str := string(file)
-
-	return str
-}
-
-func openFile(filePath string) (*os.File, bool) {
-	// Open the file
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return nil, true
+	if countWords {
+		fmt.Printf("Words: %d\n", count.words)
 	}
-
-	return file, false
+	if countChars {
+		fmt.Printf("Characters: %d\n", count.chars)
+	}
+	if countBytes {
+		fmt.Printf("Bytes: %d\n", count.bytes)
+	}
 }
